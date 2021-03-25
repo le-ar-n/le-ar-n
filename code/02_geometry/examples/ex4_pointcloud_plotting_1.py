ad9cf5070deb7fe5579815bb768084273fd69955
lidiatanasova <39670477+lidiatanasova@users.noreply.github.com>
2021-03-25 15:35:27 +0100

    restructure and update geometry examples
    remove basic geometry functions and keep object methods instead

--- code/02_geometry/examples/ex4_pointcloud_plotting_1.py
+++ code/02_geometry/examples/ex4_pointcloud_plotting_1.py
@@ -5,7 +5,9 @@ cloud = Pointcloud.from_bounds(10, 5, 0, 20)
 
 points = []
 for xyz in cloud:
-    points.append({'pos': xyz, 'radius': 0.8})
+    points.append({
+        'pos': xyz, 
+        'radius': 0.1})
 
 plotter = Plotter(figsize=(8, 5))
 plotter.draw_points(points)
--- code/02_geometry/examples/ex5_pointcloud_plotting_2.py
+++ code/02_geometry/examples/ex5_pointcloud_plotting_2.py
@@ -9,7 +9,11 @@ cloud = Pointcloud.from_bounds(10, 5, 0, 20)
 points = []
 for xyz in cloud:
     n = random()
-    points.append({'pos': xyz, 'radius': n, 'edgecolor': i_to_green(n), 'facecolor': i_to_red(n)})
+    points.append({
+        'pos': xyz, 
+        'radius': n, 
+        'edgecolor': i_to_green(n), 
+        'facecolor': i_to_red(n)})
 
 plotter = Plotter(figsize=(8, 5))
 plotter.draw_points(points)
