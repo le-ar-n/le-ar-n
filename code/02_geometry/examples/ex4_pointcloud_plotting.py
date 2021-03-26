c543d543cb79ff14b64995d4385698f8d9bb5a1a
lidiatanasova <39670477+lidiatanasova@users.noreply.github.com>
2021-03-26 14:53:19 +0100

    update pointcloud example

--- code/02_geometry/examples/ex4_pointcloud_plotting.py
+++ code/02_geometry/examples/ex4_pointcloud_plotting.py
@@ -1,20 +1,26 @@
 from random import random
 from compas.geometry import Pointcloud
+from compas.geometry import Point, Circle, Plane
 from compas_plotters import Plotter
 from compas.utilities import i_to_green
 from compas.utilities import i_to_red
 
 cloud = Pointcloud.from_bounds(10, 5, 0, 20)
-
 points = []
+cirlces = []
+
 for xyz in cloud:
+    print(xyz)
     n = random()
-    points.append({
-        'pos': xyz, 
-        'radius': n, 
-        'edgecolor': i_to_green(n), 
-        'facecolor': i_to_red(n)})
+
+    points.append(xyz)
+    
+    radius = n
+    plane = Plane(xyz, [0,0,1])
+    circle = Circle(plane, radius)
+        # 'edgecolor': i_to_green(n), 
+        # 'facecolor': i_to_red(n)})
 
 plotter = Plotter(figsize=(8, 5))
-plotter.draw_points(points)
+plotter.draw_lines(points)
 plotter.show()
