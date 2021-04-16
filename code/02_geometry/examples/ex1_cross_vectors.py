6ca5eaaa637497825cca81923dbc8b23efc1301d
lidiatanasova <39670477+lidiatanasova@users.noreply.github.com>
2021-04-16 09:37:09 +0200

    Update ex1_cross_vectors.py

--- code/02_geometry/examples/ex1_cross_vectors.py
+++ code/02_geometry/examples/ex1_cross_vectors.py
@@ -1,6 +1,6 @@
 from compas.geometry import Vector
 
-# Create two vectors
+# Create two unit vectors
 u = Vector(1.0, 0.0, 0.0)
 v = Vector(0.0, 1.0, 0.0)
 a = Vector(0.0, 0.0, 0.0)
@@ -14,6 +14,6 @@ print(uxv)
 # Print the length of the cross product
 print(uxv.length)
 
-# TO DO
+# Print if the cross product is in the positive z-direction
 print(u.cross(v)[2] > 0)
 print(v.cross(u)[2] > 0)
